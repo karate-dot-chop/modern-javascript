ec6bcde70433c6d781b305453564c054395ecb8b
dani <dani.zaghian@gmail.com>
2021-06-02 10:53:33 -0700

    adds webpack and includes bundled script in html

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-/* global moment */
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log("Hello from JS!");
 var startOfDay = moment().startOf('day').fromNow();
 console.log(startOfDay);
+console.log(math.sqrt(-4));
